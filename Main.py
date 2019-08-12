ce656837072a7c7cf41a6db7214da395ad74815d
Carlos Padilha <cpadilha38@outlook.com.br>
2019-08-12 17:51:29 -0300

    Commit 002

---
+++ Main.py
@@ -0,0 +1,4 @@
+from funcoesAuxiliares import media
+
+m = media(nome="thiago", n1=7)
+print()
---
+++ funcoesAuxiliares.py
@@ -0,0 +1,6 @@
+def media(n1=6, nome="carlos"):
+    if n1 < 6 or nome == "carlos":
+        return "se fodeu"
+    else:
+        return "aprovado"
+
